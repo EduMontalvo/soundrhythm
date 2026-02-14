c0cb8d8d7a15906c834faf85b0e47d755004d0af
Edu Montalvo <kaedu18@gmail.com>
2026-02-14 01:56:56 -0500

    add imgs

---
+++ src/data/data.ts
@@ -0,0 +1,52 @@
+import type { ImageType } from "../types/images.type";
+
+export const  AlterImage : ImageType[] = [
+    {
+        id: 1,
+        image: "../assets/imgs/img1.jpg",
+    },
+    {
+        id: 2,
+        image: "../assets/imgs/img2.jpg",
+    },
+    {
+        id: 3,
+        image: "../assets/imgs/img3.jpg",
+    },
+    {
+        id: 4,
+        image: "../assets/imgs/img4.jpg",
+    },
+    {
+        id: 5,
+        image: "../assets/imgs/img5.jpg",
+    },
+    {
+        id: 6,
+        image: "../assets/imgs/img6.jpg",
+    },
+    {
+        id: 7,
+        image: "../assets/imgs/img7.jpg",
+    },
+    {
+        id: 8,
+        image: "../assets/imgs/img8.jpg",
+    },
+    {
+        id: 9,
+        image: "../assets/imgs/img9.jpg",
+    },
+    {
+        id: 10,
+        image: "../assets/imgs/img10.jpg",
+    },
+    {
+        id: 11,
+        image: "../assets/imgs/img11.jpg",
+    },
+    {
+        id: 12,
+        image: "../assets/imgs/img12.jpg",
+    }
+]
